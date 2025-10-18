d66518dd93fc9bb779e00c9665a3d27f1ea3ee58
Shrijii <shrijigupta30@gmail.com>
2025-10-18 22:30:49 +0530

    Adding python

--- ifDemo1.py
+++ ifDemo1.py
@@ -12,40 +12,6 @@
 # else :
 #     print("it is not a leap year")
 
-# a= input("enter age : ")
-# a= int(a)
-# msg= "kid" if a<=13 else "teenager" if a<20 else "adult"
-# print (msg)
-# i=10
-# while i<11:
-#     print ("gym jao  faltu ki cheeje mt khareedo")
-#     print("sunta mai vahi jo meko krna hota hai be happy be happy buy something to be happy")
-#     print ("mai rapido kr rahi hu")
-#     print("don't try to change the person because you never loved the changed one")
-#     print("i'm not changing just tr to upgrading")
-#     print(" if the person doing drink stop him, if he doing smoke stop him if he is taking tobaku stop him "
-#           "if he is going club stop him if he is doing miss behave with you and your family stop him "
-#           "but when he is doing something good then try to support him ")
-#     print ("what if he is making chaos n to tease in daily basis? what is good in these ? ")
-#     print(" sometime people have diffrent mind set somethings are going  in side the mind that's why the person want's to go gym to make him self relaxd and wants to gain self respect again for him , beacuse the perosn you mate faced multiple problems in past and know he wants to upgrade in his on way"
-#           "where he is creating certain continuation applying restriction to make everything correct and again metnal helath and pyshical helath ")
-#     print("what's wrong with that person who is trying to cure you.? ")
-#     print("some time over watering can harm the tree some time over sun light can harm the tree , so what we learn here we have to understand what is requried and when to start and when to stop")
-#     print("first - this is not an essay that u r concluding like what we learn and the second thing - u never accecpt ur fault and the third thing is - i got it i will not going to tell u all that things again  and this is the final conclusion and i'll always love the per"
-#           "son i love  when he is the same one or he is the another one(like differnt mentality )  so dont make thing big and accept your fault donkey!")
-#     print("we all know that - things always not go into the same flow , things will up things will down and again up, "
-#           "second relationship is like a new born child - finality its like baby, teanager , adult then become mature"
-#           "and its okay we have all this steps because we are rising our own child not addopted and mature one who already learned everything")
-#     print(" it doesnt mean u act like aggressive all the time , while our time is going up or going down ,u r not getting my point or u got it from the first tym and dont try to accept or change , dont write essay here  i know everything you wrote previously just accept it")
-#     i=i+1
-#     print("may i know the time when you getup early after i come in your life, "
-#           "when we start our relationship you daily video call me at 7 o'clock and request me to go out to see the sun , but know a days you become lazy because of me so i am feeling , really i am good for you for bad for you beacuse all good things are gone after me")
-#     print("that was the tym of summer , in between there was spring , rainy , winter n now there is chilling winter , we all got lazy . it is not because of u , im a that ki d of person . if i was in my home i did the same . After coming summer i will be the same as before ")
-#     print("then consider me as weather , weather change i changed wait for the summer , you will find me the same ")
-#     print("but still u try to change me and my routine  and i'm working on that things but you don't . if you have to win the argument then i love to surrender. but do some flash light on words n what im trying to say. ")
-#     print("bye")
-# print ("done")
-
 # a,b =input().split()
 # a=int(a)
 # b=int(b)
